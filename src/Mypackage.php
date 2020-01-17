2b5bf1e1141dd0415ea710c6037ec85a00a630e0
tangzhiyuan <tangzy@x-bull.com>
2020-01-17 14:03:27 +0800

    tanlex/mypackage

---
+++ src/Mypackage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDZ
+ * Date: 2020/1/17
+ * Time: 13:47
+ */
+namespace tanlex\mypackage;
+
+class Mypackage
+{
+    public function test()
+    {
+        echo "this is composer mypackage test";
+    }
+}
